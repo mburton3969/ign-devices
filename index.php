d3276fad9a755c6c65770cd28b7d71ce16c037b6
Mike Burton <michael.burton@ignition633.org>
2020-12-03 19:26:12 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Device Donation</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.5.9/js/uikit.min.js" integrity="sha512-OZ9Sq7ecGckkqgxa8t/415BRNoz2GIInOsu8Qjj99r9IlBCq2XJlm9T9z//D4W1lrl+xCdXzq0EYfMo8DZJ+KA==" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    
+    <h1>
+      Testing
+    </h1>
+    
+  </body>
+</html>
